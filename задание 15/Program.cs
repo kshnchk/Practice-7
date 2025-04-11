616fb1ec8112f79ff36ca284094cb1deb3be74fa
kshnchk <kruglovaksusa31@gmail.com>
2025-04-11 19:27:37 +0400

    Add files via upload

---
+++ задание 15/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+Дана матрица N x M. Сжать массив, удалив из него все элементы,
+величина которых находится в интервале [a, b]. Освободившиеся в конце
+массива элементы заполнить нулями.
+*/
+
+namespace задание_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк:");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов:");
+            int M = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matrix = new int[N, M];
+            Random rand = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    matrix[i, j] = rand.Next(1, 101);
+                }
+            }
+            Console.WriteLine("Исходная матрица:");
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Введите границы интервала [a, b]:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int[] flatArray = new int[N * M];
+            int index = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    if (matrix[i, j] < a || matrix[i, j] > b)
+                    {
+                        flatArray[index++] = matrix[i, j];
+                    }
+                }
+            }
+            for (; index < flatArray.Length; index++)
+            {
+                flatArray[index] = 0;
+            }
+            Console.WriteLine("\nСжатый массив:");
+            for (int i = 0; i < flatArray.Length; i++)
+            {
+                Console.Write(flatArray[i] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ задание 17/Program.cs
@@ -0,0 +1,85 @@
+﻿/*
+Дана матрица N x M. Все элементы равные 0 поменять местами с
+элементами последней строки (если в последней строке все элементы
+равны 0, то менять местами с элементами предпоследней строки).
+*/
+
+namespace задание_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Введите количество строк:");
+                int N = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите количество столбцов:");
+                int M = Convert.ToInt32(Console.ReadLine());
+                
+                int[,] matrix = new int[N, M];
+                Random rand = new Random();
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < M; j++)
+                    {
+                        matrix[i, j] = rand.Next(-10, 11); 
+                    }
+                }
+                Console.WriteLine("Сгенерированная матрица:");
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < M; j++)
+                    {
+                        Console.Write(matrix[i, j] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+
+                int lastRowIndex = N - 1;
+                int lastNonZeroRow = lastRowIndex;
+                while (lastNonZeroRow >= 0)
+                {
+                    bool allZero = true;
+                    for (int j = 0; j < M; j++)
+                    {
+                        if (matrix[lastNonZeroRow, j] != 0)
+                        {
+                            allZero = false;
+                            break;
+                        }
+                    }
+                    if (!allZero)
+                    {
+                        break;
+                    }
+
+                    lastNonZeroRow--;
+                }
+                if (lastNonZeroRow < 0) return;
+
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < M; j++)
+                    {
+                        if (matrix[i, j] == 0)
+                        {
+                            int temp = matrix[i, j];
+                            matrix[i, j] = matrix[lastNonZeroRow, j];
+                            matrix[lastNonZeroRow, j] = temp;
+                        }
+                    }
+                }
+                Console.WriteLine("\nМатрица после замены:");
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < M; j++)
+                    {
+                        Console.Write(matrix[i, j] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
